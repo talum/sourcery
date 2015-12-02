e835e611a94b912649a7a4436295c230f39d7358
medwards1771 <meredith.edwards1771@gmail.com>
2015-12-02 14:26:48 -0500

    saved email from google+ login and add firstnmae lastname methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.gmail_name = auth.info.name
+      user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
+  end
+
+  def firstname
+    self.gmail_name.split(" ").first
+  end
+
+  def lastname
+    self.gmail_name.split(" ").last
   end
 
 end
