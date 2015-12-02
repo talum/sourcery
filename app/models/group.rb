5d47c4193c31ed4ad2bb22fcb2f172a43c2d26ba
jrectenwald <jimrec101@gmail.com>
2015-12-02 09:36:54 -0500

    resource and group relations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ActiveRecord::Base
+  belongs_to :teacher
+  has_many :student_groups
+  has_many :students, through: :student_groups
+  has_many :resources
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,6 @@
 class Resource < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :user
+  has_many :comments
+  has_many :favorites
 end
