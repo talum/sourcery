68448e423f235ba56839aba15af6d9ae4854c6cd
talum <tracidini@gmail.com>
2015-12-02 17:21:07 -0500

    Adds first comment test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#valid?' do
+    let(:comment) {  FactoryGirl.build( :comment, content: content)}
+    let(:content){'hellloo'}
+
+    context 'when the comment is empty' do
+      let(:content) {nil}
+      it 'is invalid' do
+        expect(comment).to_not be_valid
+      end
+    end
+  end
+
 end
