4e5d4f7a00addf42c1b876e6b9eacb3d12dae65f
medwards1771 <meredith.edwards1771@gmail.com>
2015-12-02 18:00:47 -0500

    took out binding.pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    binding.pry
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     unless @current_user
       User.find(params[:id]) if params[:id]
