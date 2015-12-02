3d42c41bcd18f8134008aa469467ea05dc39e242
jrectenwald <jimrec101@gmail.com>
2015-12-02 13:05:57 -0500

    add groups/resources controllers and routes

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,23 @@
+class GroupsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/models/student_group.rb
+++ app/models/student_group.rb
@@ -1,2 +1,4 @@
 class StudentGroup < ActiveRecord::Base
+  belongs_to :student
+  belongs_to :group
 end
