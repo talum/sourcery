43abe2cf1082e26942aa563e541d8c57451b0bd8
talum <tracidini@gmail.com>
2015-12-05 12:31:55 -0500

    Cleans up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    @resource = Resource.find(comment_params[:resource_id])
     if @comment.save
       html_string = render_to_string 'comments/_comment', locals: {comment: @comment}, layout: false
       render json: {template: html_string}
@@ -15,7 +14,6 @@ class CommentsController < ApplicationController
     @resource = @comment.resource
     @comment.destroy
     redirect_to @resource
-
   end
 
 private 
