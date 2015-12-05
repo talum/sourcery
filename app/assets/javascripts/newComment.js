b4c2b6a981f000178d2702d2d299f1437554df7e
talum <tracidini@gmail.com>
2015-12-05 11:41:03 -0500

    Renames container to comment-container for Ajax

--- app/assets/javascripts/newComment.js
+++ app/assets/javascripts/newComment.js
@@ -1,7 +1,7 @@
 $(function(){
  $('#new-comment-js').on('ajax:success', function(event, data, status, xhr){
    var template = $(data.template).wrap("<li></li>");
-   $('.container').append(template);
+   $('.comment-container').append(template);
    $(this.find('input:text')).val('')
  })
 })
