8f8229c1aeafb222dc953a0a1a7c346cb8c43cde
mford22392 <mford22392@gmail.com>
2015-12-06 12:35:56 -0500

    clears comment box after submitting comment

--- app/assets/javascripts/newComment.js
+++ app/assets/javascripts/newComment.js
@@ -2,6 +2,6 @@ $(function(){
  $('#new-comment-js').on('ajax:success', function(event, data, status, xhr){
    var template = $(data.template).wrap("<li></li>");
    $('.comment-container').append(template);
-   $(this.find('input:text')).val('')
+   $('.form-control').val('')
  })
 })
