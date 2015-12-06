e4d4676a75dcf3778db07a39c897d68b4d6321f6
talum <tracidini@gmail.com>
2015-12-06 10:20:17 -0500

    Clears input field after search

--- app/assets/javascripts/nyt_articlesearch.js
+++ app/assets/javascripts/nyt_articlesearch.js
@@ -5,6 +5,7 @@ $(document).on("ready", function(){
 
 function searchListener(){
   $("#article-search").on("ajax:success", function(event, data){
+    $(this).find("input:text").val('');
     var articleHtmlArray = []; 
     for(var i=0; i<data.length; i++){
       var linkHTML = "<a href='"+data[i].link+"' target='_blank'>"+data[i].title+"</a><br>";
