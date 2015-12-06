d3aae3f1a40b01988d669d02b6f439fcba1ee145
talum <tracidini@gmail.com>
2015-12-06 14:12:46 -0500

    Refactors to use partials instead of straight JS

--- app/assets/javascripts/nyt_articlesearch.js
+++ app/assets/javascripts/nyt_articlesearch.js
@@ -1,17 +1,18 @@
 $(document).on("ready", function(){
   searchListener();
+  clearListener();
 
 });
 
 function searchListener(){
   $("#article-search").on("ajax:success", function(event, data){
     $(this).find("input:text").val('');
-    var articleHtmlArray = []; 
-    for(var i=0; i<data.length; i++){
-      var linkHTML = "<a href='"+data[i].link+"' target='_blank'>"+data[i].title+"</a><br>";
-      articleHtmlArray.push(linkHTML);
-    }
-    var finalHTML = articleHtmlArray.join("");
-    $("#article-results").append(finalHTML);
+    $("#article-results").append(data.article_list);
   });
+}
+
+function clearListener(){
+  $("#clear-search-results-button").on("click", function(){
+    $("#article-results").html("");
+  })
 }
--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -3,7 +3,10 @@ class QueriesController < ApplicationController
   def create
     term = params[:q]
     articles = Adapters::ArticleClient.new.search(term)
-    render json: articles
+    article_list = articles.map do |article|
+      render_to_string(partial: "groups/query_results", locals: {article: article})
+    end.join()
+    render json: {article_list: article_list}
   end
 
 end
