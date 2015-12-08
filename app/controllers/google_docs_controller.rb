b26e17ef0867264ca8bb4a75fb613ff29d04e3b1
medwards1771 <meredith.edwards1771@gmail.com>
2015-12-08 11:07:45 -0500

    put google_doc form in partial

--- app/controllers/google_docs_controller.rb
+++ app/controllers/google_docs_controller.rb
@@ -3,9 +3,9 @@ class GoogleDocsController < ApplicationController
   def create
     session = GoogleDrive.login_with_oauth(current_user.oauth_token)
     group_name = Group.find(doc_params[:group_id]).topic
-    folder = session.collection_by_title(group_name)
-    binding.pry
-    # folder = session.root_collection.create_subcollection(group_name) unless folder
+    # folder = session.collection_by_title(group_name)
+    # binding.pry
+    folder = session.root_collection.create_subcollection(group_name)
     
     if doc_params[:doc_type] == "Document"
       remote_doc = session.upload_from_string("Collaborate here!", title=doc_params[:title], :content_type => "text/plain")
