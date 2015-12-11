d9b3eac05ba893f672f0a086c9cd0ceb9e2e536d
talum <tracidini@gmail.com>
2015-12-11 09:31:01 -0500

    Cleans up comments and 404 page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,10 +1,6 @@
 class TeachersController < ApplicationController
 
   def show
-    # @top_commenter = Student.with_most_comments
-    # @top_contributor = Student.with_most_resources
-    # @student_comments = Student.order_by_comments_name[0..9]
-    # @student_resources = Student.order_by_resources_name[0..9]
     @top_commenter = Teacher.find_by(user_id: current_user.id).top_student_commenter
     @top_contributor = Teacher.find_by(user_id: current_user.id).top_student_contributor
     @student_comments = Teacher.find_by(user_id: current_user.id).student_comments[0..9]
--- app/models/student.rb
+++ app/models/student.rb
@@ -14,23 +14,16 @@ class Student < ActiveRecord::Base
   has_many :comments, through: :user
   has_many :resources, through: :user
 
-  ###########
-  #COMMENTS#
-  ##########
-
-  #Helper Method:
   def self.array_of_num_comments_by_student 
     joins(:comments).group('students.id').count.sort_by{|k, v| v}.reverse
   end
 
-  #Array of hashes that include a student *object* and number of comments they have made:
   def self.order_by_comments_object
     self.array_of_num_comments_by_student.each_with_object([]) do |student_array, array_object|
       array_object << {student: Student.find(student_array.first), comment_count: student_array.last}
     end
   end
 
-  #Array of hashes that include a student's *name* and number of comments they have made:
   def self.order_by_comments_name
     self.array_of_num_comments_by_student.each_with_object([]) do |student_array, array_object|
       array_object << {name: Student.find(student_array.first).user.gmail_name, comment_count: student_array.last}
@@ -47,7 +40,6 @@ class Student < ActiveRecord::Base
     end
   end
 
-  #return the student object who has commented the most
   def self.with_most_comments
     if self.array_of_num_comments_by_student.length > 0 && self.array_of_num_comments_by_student.length > 0
       {student: Student.find(self.array_of_num_comments_by_student.first.first), num_comments: self.array_of_num_comments_by_student.first.last}
@@ -65,23 +57,16 @@ class Student < ActiveRecord::Base
     (counter / self.comments.length) if self.comments.length > 0
   end
 
-  ###########
-  #RESOURCES#
-  ###########
-
-  #Helper Method:
   def self.array_of_num_of_resources_by_student
     joins(:resources).group('students.id').count.sort_by{|k, v| v}.reverse
   end
 
-  #Array of hashes that include a student *object* and number of resources they have made:
   def self.order_by_resources_object
     self.array_of_num_of_resources_by_student.each_with_object([]) do |student_array, array_object|
       array_object << {student: Student.find(student_array.first), resource_count: student_array.last}
     end
   end
 
-  #Array of hashes that include a student's *name* and number of resources they have made:
   def self.order_by_resources_name
     self.array_of_num_of_resources_by_student.each_with_object([]) do |student_array, array_object|
       array_object << {student: Student.find(student_array.first).user.gmail_name, resource_count: student_array.last}
@@ -99,7 +84,6 @@ class Student < ActiveRecord::Base
     array
   end
 
-  #Returns the student object who has made the most resources
   def self.with_most_resources
     if self.array_of_num_of_resources_by_student.length > 0 && self.array_of_num_of_resources_by_student.length > 0
     {student: Student.find(self.array_of_num_of_resources_by_student.first.first), num_resources: self.array_of_num_of_resources_by_student.first.last}
